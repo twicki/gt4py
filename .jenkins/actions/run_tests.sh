999c5207a44ace46f8fe3ed663f90489bd10f55c
eddie-c-davis <22508135+eddie-c-davis@users.noreply.github.com>
2020-09-09 12:35:26 -0700

    Update run_tests.sh

--- .jenkins/actions/run_tests.sh
+++ .jenkins/actions/run_tests.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-module load /project/d107/install/modulefiles/boost/1_74_0 
+if [[ $HOSTNAME =~ .*daint.* ]]
+then
+    module load /project/d107/install/modulefiles/boost/1_74_0
+else
+    export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-10.2/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
+fi
 python3 -m pip install --user virtualenv
 python3 -m venv gt4pyenv
 source gt4pyenv/bin/activate
 # pip3 install --upgrade wheel # might need this later
 git clone https://github.com/VulcanClimateModeling/gt4py.git gt4py
-if [[ $HOSTNAME =~ .*daint.* ]]
-then
-    pip3 install cupy-cuda101==7.7.0
-    pip3 install --no-cache-dir -e gt4py[cupy-cuda101]
-else
-    pip3 install cupy-cuda102==7.7.0
-    pip3 install --no-cache-dir -e gt4py[cupy-cuda102]
-fi
+pip3 install cupy-cuda101==7.7.0
+pip3 install --no-cache-dir -e gt4py[cupy-cuda101]
 python3 -m gt4py.gt_src_manager install
 python3 -m pytest gt4py
