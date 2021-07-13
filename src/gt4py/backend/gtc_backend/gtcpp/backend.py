61dc99f6f2f0e04901c50c766ff8b1b507e09e04
Eddie Davis <eddied@vulcan.com>
2021-07-13 10:48:03 -0700

    Skip graph_merge_horizontal_executions and KCacheDetection in GTCpp and CUDA backends

--- src/gt4py/backend/gtc_backend/gtcpp/backend.py
+++ src/gt4py/backend/gtc_backend/gtcpp/backend.py
@@ -37,8 +37,10 @@ from gtc import gtir_to_oir
 from gtc.common import DataType
 from gtc.gtcpp import gtcpp, gtcpp_codegen, oir_to_gtcpp
 from gtc.passes.gtir_pipeline import GtirPipeline
-from gtc.passes.oir_optimizations.caches import FillFlushToLocalKCaches
-from gtc.passes.oir_optimizations.horizontal_execution_merging import GreedyMerging
+from gtc.passes.oir_dace_optimizations.horizontal_execution_merging import (
+    graph_merge_horizontal_executions,
+)
+from gtc.passes.oir_optimizations.caches import FillFlushToLocalKCaches, KCacheDetection
 from gtc.passes.oir_pipeline import OirPipeline
 
 
@@ -55,7 +57,11 @@ class GTCGTExtGenerator:
     def __call__(self, definition_ir) -> Dict[str, Dict[str, str]]:
         gtir = GtirPipeline(DefIRToGTIR.apply(definition_ir)).full()
         oir = OirPipeline(gtir_to_oir.GTIRToOIR().visit(gtir)).full(
-            skip=[GreedyMerging().visit, FillFlushToLocalKCaches().visit]
+            skip=[
+                graph_merge_horizontal_executions,
+                KCacheDetection().visit,
+                FillFlushToLocalKCaches().visit,
+            ]
         )
         gtcpp = oir_to_gtcpp.OIRToGTCpp().visit(oir)
         implementation = gtcpp_codegen.GTCppCodegen.apply(
