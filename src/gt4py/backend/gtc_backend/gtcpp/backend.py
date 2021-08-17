72f0ceaa02cfb4615c775466067db2b04e21a3b9
Eddie Davis <eddied@vulcan.com>
2021-08-17 17:14:40 +0000

    Merge remote branch 'eddied/update-skip-passes' into develop-v34

--- src/gt4py/backend/gtc_backend/gtcpp/backend.py
+++ src/gt4py/backend/gtc_backend/gtcpp/backend.py
@@ -38,6 +38,7 @@ from gtc.common import DataType
 from gtc.gtcpp import gtcpp, gtcpp_codegen, oir_to_gtcpp
 from gtc.passes.gtir_pipeline import GtirPipeline
 from gtc.passes.oir_optimizations.caches import FillFlushToLocalKCaches, KCacheDetection
+from gtc.passes.oir_optimizations.horizontal_execution_merging import OnTheFlyMerging
 from gtc.passes.oir_pipeline import OirPipeline
 
 
@@ -54,7 +55,11 @@ class GTCGTExtGenerator:
     def __call__(self, definition_ir) -> Dict[str, Dict[str, str]]:
         gtir = GtirPipeline(DefIRToGTIR.apply(definition_ir)).full()
         oir = OirPipeline(gtir_to_oir.GTIRToOIR().visit(gtir)).full(
-            skip=[KCacheDetection, FillFlushToLocalKCaches]
+            skip=[
+                OnTheFlyMerging,
+                KCacheDetection,
+                FillFlushToLocalKCaches,
+            ]
         )
         gtcpp = oir_to_gtcpp.OIRToGTCpp().visit(oir)
         implementation = gtcpp_codegen.GTCppCodegen.apply(
