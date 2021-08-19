06f6ebc6680b301bd2464b4a127cd6629a964c22
Eddie Davis <eddied@vulcan.com>
2021-08-19 14:57:19 +0000

    Add GreedyMerging to skip lists

--- src/gt4py/backend/gtc_backend/cuda/backend.py
+++ src/gt4py/backend/gtc_backend/cuda/backend.py
@@ -32,8 +32,11 @@ from gtc import gtir_to_oir
 from gtc.common import DataType
 from gtc.cuir import cuir, cuir_codegen, extent_analysis, kernel_fusion, oir_to_cuir
 from gtc.passes.gtir_pipeline import GtirPipeline
+from gtc.passes.oir_dace_optimizations.horizontal_execution_merging import (
+    graph_merge_horizontal_executions,
+)
 from gtc.passes.oir_optimizations.caches import KCacheDetection
-from gtc.passes.oir_optimizations.horizontal_execution_merging import OnTheFlyMerging
+from gtc.passes.oir_optimizations.horizontal_execution_merging import GreedyMerging, OnTheFlyMerging
 from gtc.passes.oir_optimizations.pruning import NoFieldAccessPruning
 from gtc.passes.oir_pipeline import OirPipeline
 
@@ -51,7 +54,13 @@ class GTCCudaExtGenerator:
     def __call__(self, definition_ir) -> Dict[str, Dict[str, str]]:
         gtir = GtirPipeline(DefIRToGTIR.apply(definition_ir)).full()
         oir = OirPipeline(gtir_to_oir.GTIRToOIR().visit(gtir)).full(
-            skip=[OnTheFlyMerging, KCacheDetection, NoFieldAccessPruning]
+            skip=[
+                # graph_merge_horizontal_executions,
+                GreedyMerging,
+                OnTheFlyMerging,
+                KCacheDetection,
+                NoFieldAccessPruning
+            ]
         )
         cuir = oir_to_cuir.OIRToCUIR().visit(oir)
         cuir = kernel_fusion.FuseKernels().visit(cuir)
